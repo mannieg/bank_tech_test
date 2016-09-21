761220077bfd0e6b37d4333e37f9bb534bd91946
Manjinder Gill <manjinder.s.gill@googlemail.com>
2016-09-21 14:49:56 +0100

    Refactored tests to follow DRY

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -5,24 +5,20 @@ describe Account do
 
   before do
     allow($stdout).to receive(:puts)
+    allow(Time).to receive(:now).and_return(Time.parse('2012-01-10'), Time.parse('2012-01-13'), Time.parse('2012-01-14'))
   end
 
   it 'can deposit an amount with given date' do
-    allow(Time).to receive(:now).and_return Time.parse '2012-01-10'
     expect{ account.deposit(1000) }.to change{ account.balance }.by(1000)
   end
 
   it 'allows withdrawal of a given amount on a specific date' do
-    allow(Time).to receive(:now).and_return Time.parse '2012-01-14'
     expect{ account.withdraw(500) }.to change{ account.balance }.by(-500)
   end
 
   it 'returns the historical transactions' do
-    allow(Time).to receive(:now).and_return Time.parse '2012-01-10'
     account.deposit(1000)
-    allow(Time).to receive(:now).and_return Time.parse '2012-01-13'
     account.deposit(2000)
-    allow(Time).to receive(:now).and_return Time.parse '2012-01-14'
     account.withdraw(500)
 
     example_arr = [['10/01/2012', 1000,nil,1000], ['13/01/2012',2000,nil,3000],
